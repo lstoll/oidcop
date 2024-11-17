390c6f83121b398cf48627f6c255816523f0cf7a
Lincoln Stoll <me@lds.li>
2024-11-17 14:04:37 +0100

    update the example OP as well

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # oidcop
 
 Go library for implementing OIDC OPs (Servers)
+
+## Example OP
+
+An example server is provided. Can be run:
+
+```
+go run ./cmd/oidc-example-op
+```
+
+And interacted with via
+
+```
+go run github.com/lstoll/oidc/cmd/oidccli -issuer=http://localhost:8085 -client-id=cli -scopes=openid,offline_access -skip-cache info
+```
--- cmd/oidc-example-op/main.go
+++ cmd/oidc-example-op/main.go
@@ -4,20 +4,24 @@ import (
 	_ "embed"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"time"
 
-	"github.com/lstoll/oidc"
 	"github.com/lstoll/oidcop"
-	"github.com/lstoll/oidcop/discovery"
 	"github.com/lstoll/oidcop/staticclients"
+	"github.com/lstoll/oidcop/storage"
 )
 
 //go:embed clients.json
 var clientsJSON []byte
 
 func main() {
-	smgr := newStubSMGR()
-	privh, pubh := mustInitKeyset()
+	store, err := storage.NewJSONFile(filepath.Join(os.TempDir(), "oidc-example-op.json"))
+	if err != nil {
+		log.Fatalf("creating storage: %v", err)
+	}
+	privh, _ := mustInitKeyset()
 
 	clients, err := staticclients.ExpandUnmarshal(clientsJSON)
 	if err != nil {
@@ -26,36 +30,28 @@ func main() {
 
 	iss := "http://localhost:8085"
 
-	core, err := oidcop.New(&oidcop.Config{
-		Issuer:           iss,
-		AuthValidityTime: 5 * time.Minute,
-		CodeValidityTime: 5 * time.Minute,
-	}, smgr, clients, oidcop.NewStaticKeysetHandle(privh))
+	svr := &server{}
+
+	core, err := oidcop.New(
+		iss,
+		store,
+		clients,
+		map[oidcop.SigningAlg]oidcop.HandleFn{
+			oidcop.SigningAlgRS256: oidcop.StaticHandleFn(privh),
+		},
+		svr,
+		&oidcop.Options{
+			Issuer:           iss,
+			AuthValidityTime: 5 * time.Minute,
+			CodeValidityTime: 5 * time.Minute,
+		})
 	if err != nil {
 		log.Fatalf("Failed to create OIDC server instance: %v", err)
 	}
 
 	m := http.NewServeMux()
-
-	svr := &server{
-		oidc:            core,
-		storage:         smgr,
-		tokenValidFor:   30 * time.Second,
-		refreshValidFor: 5 * time.Minute,
-	}
-
-	m.Handle("/", svr)
-
-	md := discovery.DefaultCoreMetadata(iss)
-	md.AuthorizationEndpoint = iss + "/auth"
-	md.TokenEndpoint = iss + "/token"
-
-	discoh, err := discovery.NewConfigurationHandler(md, oidc.NewStaticPublicHandle(pubh))
-	if err != nil {
-		log.Fatalf("Failed to initialize discovery handler: %v", err)
-	}
-	m.Handle("GET /.well-known/openid-configuration", discoh)
-	m.Handle("GET /.well-known/jwks.json", discoh)
+	core.AttachHandlers(m, nil)
+	m.HandleFunc("/finish", svr.finishAuthorization)
 
 	log.Printf("Listening on: %s", "localhost:8085")
 	err = http.ListenAndServe("localhost:8085", m)
